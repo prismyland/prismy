8a032bff3e0e4ab35d6c3148fc3653b6e06654a6
Junyoung Choi <fluke8259@gmail.com>
2019-06-10 00:53:29 +0900

    Implement RedirectResult

--- src/BaseHandler.ts
+++ src/BaseHandler.ts
@@ -1,7 +1,8 @@
-import { SendResult, SendResultOptions } from './results/SendResult'
-import { Selector } from './createInjectDecorators'
 import { IncomingMessage, ServerResponse } from 'http'
 import { sendError } from 'micro'
+import { SendResult, SendResultOptions } from './results/SendResult'
+import { RedirectResultOptions, RedirectResult } from './results/RedirectResult'
+import { Selector } from './createInjectDecorators'
 
 export class BaseHandler {
   context?: {
@@ -11,6 +12,11 @@ export class BaseHandler {
 
   send<D = any>(data?: D, options?: SendResultOptions) {
     return new SendResult(data, options)
+  }
+
+  redirect(location: string, options?: RedirectResultOptions) {
+    console.log(location)
+    return new RedirectResult(location, options)
   }
 
   select<P>(selector: Selector<P>): P {
