2b80d0606464f4afdcaa9bfa61e6a1d49a2b6d2b
Junyoung Choi <fluke8259@gmail.com>
2019-08-06 14:13:02 +0900

    Check ContentType header when parsing json body
    To prevent CSRF attack

--- src/selectors/jsonBody.ts
+++ src/selectors/jsonBody.ts
@@ -1,8 +1,10 @@
-import { json } from 'micro'
+import { json, createError } from 'micro'
 import { Selector } from '../types'
 import { createInjectDecorators } from '../createInjectDecorators'
+import { createHeaderSelector } from './header'
 
 export interface JsonBodySelectorOptions {
+  skipContentTypeCheck?: boolean
   limit?: string | number
   encoding?: string
 }
@@ -10,11 +12,27 @@ export interface JsonBodySelectorOptions {
 export function createJsonBodySelector(
   options?: JsonBodySelectorOptions
 ): Selector<Promise<any>> {
-  return ({ req }) => {
-    return json(req, options)
+  return context => {
+    const { skipContentTypeCheck = false } = options || {}
+    if (!skipContentTypeCheck) {
+      const contentType = createHeaderSelector('content-type')(context)
+      if (!isContentTypeIsApplicationJSON(contentType as string | undefined)) {
+        throw createError(
+          400,
+          `Content type must be application/json. (Current: ${contentType})`
+        )
+      }
+    }
+    return json(context.req, options)
   }
 }
 
 export function JsonBody(options?: JsonBodySelectorOptions) {
   return createInjectDecorators(createJsonBodySelector(options))
+}
+
+function isContentTypeIsApplicationJSON(contentType: string | undefined) {
+  if (typeof contentType !== 'string') return false
+  if (!contentType.startsWith('application/json')) return false
+  return true
 }
--- src/specs/selectors/jsonBody.spec.ts
+++ src/specs/selectors/jsonBody.spec.ts
@@ -26,4 +26,60 @@ describe('JsonBody', () => {
       })
     })
   })
+
+  it('throws when content-type is not application/json', async () => {
+    class MyHandler {
+      handle(@JsonBody() body: any) {
+        return body
+      }
+    }
+
+    await testServer(MyHandler, async url => {
+      const response = await got(url, {
+        body: JSON.stringify({
+          message: 'Hello, World!'
+        }),
+        headers: {
+          'content-type': 'text/plain'
+        },
+        throwHttpErrors: false
+      })
+
+      expect(response).toMatchObject({
+        statusCode: 400,
+        body: 'Content type must be application/json. (Current: text/plain)'
+      })
+    })
+  })
+
+  it('skip content-type checking', async () => {
+    class MyHandler {
+      handle(
+        @JsonBody({
+          skipContentTypeCheck: true
+        })
+        body: any
+      ) {
+        return body
+      }
+    }
+
+    await testServer(MyHandler, async url => {
+      const response = await got(url, {
+        body: JSON.stringify({
+          message: 'Hello, World!'
+        }),
+        headers: {
+          'content-type': 'text/plain'
+        }
+      })
+
+      expect(response).toMatchObject({
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Hello, World!'
+        })
+      })
+    })
+  })
 })
