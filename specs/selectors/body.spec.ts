8106c43361684c4f1e7a020e9530142ee0aad120
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-10-21 23:16:16 +0900

    Apply prettier

--- specs/selectors/body.spec.ts
+++ specs/selectors/body.spec.ts
@@ -7,12 +7,9 @@ describe('createBodySelector', () => {
   it('returns text body', async () => {
     expect.hasAssertions()
     const bodySelector = createBodySelector()
-    const handler = prismy(
-      [bodySelector],
-      (body) => {
-        return res(`${body.constructor.name}: ${body}`)
-      }
-    )
+    const handler = prismy([bodySelector], body => {
+      return res(`${body.constructor.name}: ${body}`)
+    })
 
     await testHandler(handler, async url => {
       const response = await got(url, {
@@ -30,12 +27,9 @@ describe('createBodySelector', () => {
   it('returns parsed url encoded body', async () => {
     expect.hasAssertions()
     const bodySelector = createBodySelector()
-    const handler = prismy(
-      [bodySelector],
-      (body) => {
-        return res(body)
-      }
-    )
+    const handler = prismy([bodySelector], body => {
+      return res(body)
+    })
 
     await testHandler(handler, async url => {
       const response = await got(url, {
@@ -58,12 +52,9 @@ describe('createBodySelector', () => {
   it('returns JSON object body', async () => {
     expect.hasAssertions()
     const bodySelector = createBodySelector()
-    const handler = prismy(
-      [bodySelector],
-      (body) => {
-        return res(body)
-      }
-    )
+    const handler = prismy([bodySelector], body => {
+      return res(body)
+    })
 
     await testHandler(handler, async url => {
       const target = {
