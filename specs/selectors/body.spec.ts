87c399194628100072b8d9659ad8d07dda2f12aa
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-10-21 23:03:50 +0900

    Add a test case which returns parsed url encoded body

--- specs/selectors/body.spec.ts
+++ specs/selectors/body.spec.ts
@@ -7,11 +7,12 @@ describe('createBodySelector', () => {
   it('returns text body', async () => {
     expect.hasAssertions()
     const bodySelector = createBodySelector()
-    const handler = prismy([bodySelector], (body) => {
-      return res(`${body.constructor.name}: ${body}`, 200, {
-        'content-type': 'text/plain'
-      })
-    })
+    const handler = prismy(
+      [bodySelector],
+      (body) => {
+        return res(`${body.constructor.name}: ${body}`)
+      }
+    )
 
     await testHandler(handler, async url => {
       const response = await got(url, {
@@ -26,12 +27,43 @@ describe('createBodySelector', () => {
     })
   })
 
+  it('returns parsed url encoded body', async () => {
+    expect.hasAssertions()
+    const bodySelector = createBodySelector()
+    const handler = prismy(
+      [bodySelector],
+      (body) => {
+        return res(body)
+      }
+    )
+
+    await testHandler(handler, async url => {
+      const response = await got(url, {
+        method: 'POST',
+        responseType: 'json',
+        form: {
+          message: 'Hello, World!'
+        }
+      })
+
+      expect(response).toMatchObject({
+        statusCode: 200,
+        body: {
+          message: 'Hello, World!'
+        }
+      })
+    })
+  })
+
   it('returns JSON object body', async () => {
     expect.hasAssertions()
     const bodySelector = createBodySelector()
-    const handler = prismy([bodySelector], (body) => {
-      return res(body)
-    })
+    const handler = prismy(
+      [bodySelector],
+      (body) => {
+        return res(body)
+      }
+    )
 
     await testHandler(handler, async url => {
       const target = {
