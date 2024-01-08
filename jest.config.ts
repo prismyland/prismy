0cda40a2d6239e904f3d9bc11c9f61682751a0d2
Junyoung Choi <fluke8259@gmail.com>
2024-01-08 09:00:39 +0900

    Reconfigure jest for fast testing

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/examples/'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
+}
+
+export default jestConfig
